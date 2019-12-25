91208f2403767473ebfe1b42d872b0f547ce17a2
Stefan Puia <stefan.puia13@gmail.com>
2019-12-25 15:27:44 +0200

    added request logging

--- index.ts
+++ index.ts
@@ -1,17 +1,23 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { gameController } from "./src/app/api/game.controller";
 import { GameFactory } from "./src/app/game/game.factory";
 const app = express();
 
-app.use("/info", (req: Request, res: Response) => {
-    res.json(GameFactory.getAll());
-})
 
 app.use("/static", express.static("static"));
+app.get("/favicon.ico", (req: Request, res: Response, next: NextFunction) => {
+    res.sendStatus(404);
+})
+
+app.use("*", (req: Request, res: Response, next: NextFunction) => {
+    console.log(new Date(), req.method, req.originalUrl);
+    next();
+})
 app.use("/play/:game", express.static("game"));
 app.use("/game", gameController);
+
 app.use("*", (req: Request, res: Response) => {
-    res.redirect("/play/" + GameFactory.generateKey());
+    res.redirect(`/play/${GameFactory.generateKey()}/`);
 })
 
 app.listen(8080, () => { console.log("listening") });
